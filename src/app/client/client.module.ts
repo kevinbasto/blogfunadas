f1bb3e797e4fddbc053f4cbcb51d6435190cc91d
Kevin Basto <neverknowpeople@hotmail.com>
2021-08-07 16:52:58 -0500

    components added for the client module

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
+import { CataloguesComponent } from './catalogues/catalogues.component';
+import { NovelComponent } from './novel/novel.component';
+import { ChapterComponent } from './chapter/chapter.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CataloguesComponent, NovelComponent, ChapterComponent, ProfileComponent],
   imports: [
     CommonModule,
     ClientRoutingModule
