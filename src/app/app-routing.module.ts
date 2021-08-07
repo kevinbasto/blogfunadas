c9ded8f7802c3086c8606c597f4fd58c9bcff46f
Kevin Basto <neverknowpeople@hotmail.com>
2021-08-07 16:52:40 -0500

    routing implemented inside the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientComponent } from './client/client.component';
+import { ClientModule } from "./client/client.module";
+import { AdminComponent } from './admin/admin.component';
+import { AdminModule } from "./admin/admin.module";
+import { AuthComponent } from './auth/auth.component';
+import { AuthModule } from "./auth/auth.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path : 'auth',
+    component: AuthComponent,
+    children: [{
+        path: '',
+        loadChildren: './auth/auth.module#AuthModule'
+      }]
+  },
+  {
+    path : 'client',
+    component: ClientComponent,
+    children: [{
+        path : '',
+        loadChildren: './client/client.module#ClientModule'
+      }],
+  },
+  {
+    path : 'admin',
+    component: AdminComponent,
+    children: [{
+        path: '',
+        loadChildren: './admin/admin.module#AdminModule'
+      }]
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
