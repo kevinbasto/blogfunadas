7cb791f0288d27c02bdf8eba04488d4cc08563f8
Kevin Basto <neverknowpeople@hotmail.com>
2021-08-07 16:23:33 -0500

    created navigation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    AdminComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
