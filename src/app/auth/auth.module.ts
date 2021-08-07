0ad0b45aa1cd796dd0eea5b50558969c836e30fd
Kevin Basto <neverknowpeople@hotmail.com>
2021-08-07 16:52:48 -0500

    components added for the auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecoverComponent } from './recover/recover.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent, RecoverComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
