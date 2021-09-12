7c28f5b5ad75579b679a74c9babfec335d381364
Kevin <bastokevindaniel@gmail.com>
2021-09-12 11:56:52 -0500

    register and recover interfaces added to their respective services

--- src/app/core/services/auth/recover.ts
+++ src/app/core/services/auth/recover.ts
@@ -1,2 +1,3 @@
 export interface Recover {
+    recoverEmailAccount( email : string ) : Promise<any>;
 }
--- src/app/core/services/auth/register.ts
+++ src/app/core/services/auth/register.ts
@@ -1,6 +1,5 @@
 import { RegisterDto } from "../../interfaces/auth/register.interface";
 
 export interface Register {
-    
     emailRegister(register : RegisterDto) : Promise<any>;
 }
