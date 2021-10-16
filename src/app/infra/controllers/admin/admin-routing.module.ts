e606df48d9d2cc74f2d4aef75cbe78f6d30c4673
Kevin <bastokevindaniel@gmail.com>
2021-10-16 17:21:02 -0500

    admin routing refactor

--- src/app/infra/controllers/admin/admin-routing.module.ts
+++ src/app/infra/controllers/admin/admin-routing.module.ts
@@ -1,57 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TableComponent } from './table/table.component';
-import { GenreFormComponent } from './genre-form/genre-form.component';
-import { NovelFormComponent } from './novel-form/novel-form.component';
-import { ChapterFormComponent } from './chapter-form/chapter-form.component';
-import { CommentFormComponent } from './comment-form/comment-form.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { TermsFormComponent } from './terms-form/terms-form.component';
-import { PrivacyFormComponent } from './privacy-form/privacy-form.component';
+
 
 const routes : Routes = [
   {
     path : '',
     redirectTo: 'dashboard',
     pathMatch: 'full'
-  },
-  {
-    path: 'dashboard',
-    component : DashboardComponent
-  },
-  {
-    path: ':table',
-    component: TableComponent
-  },
-  {
-    path : ':table/user/:user',
-    component: UserFormComponent
-  },
-  {
-    path: ':table/genre/:genre',
-    component: GenreFormComponent
-  },
-  {
-    path: ':table/novel/:novel',
-    component: NovelFormComponent
-  },
-  {
-    path: ':table/novel/:novel/:chapter',
-    component: ChapterFormComponent
-  },
-  {
-    path: ':table/reports/:comment',
-    component: CommentFormComponent
-  },
-  {
-    path : ':table/terms/:terms',
-    component: TermsFormComponent
-  },
-  {
-    path : ':table/privacy/:privacy',
-    component: PrivacyFormComponent
   },
   {
     path : '**',
--- src/app/infra/controllers/admin/admin.module.ts
+++ src/app/infra/controllers/admin/admin.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TableComponent } from './table/table.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { GenreFormComponent } from './genre-form/genre-form.component';
-import { NovelFormComponent } from './novel-form/novel-form.component';
-import { ChapterFormComponent } from './chapter-form/chapter-form.component';
-import { CommentFormComponent } from './comment-form/comment-form.component';
+
+
+
+
+// material imports
 import { MatCardModule } from "@angular/material/card";
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from "@angular/material/input";
@@ -15,22 +13,11 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from '@angular/material/sort';
-import { TermsFormComponent } from './terms-form/terms-form.component';
-import { PrivacyFormComponent } from './privacy-form/privacy-form.component';
+
 import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    TableComponent,
-    UserFormComponent,
-    GenreFormComponent,
-    NovelFormComponent,
-    ChapterFormComponent,
-    CommentFormComponent,
-    TermsFormComponent,
-    PrivacyFormComponent,
-  ],
+  declarations: [],
   imports: [
     CommonModule, 
     AdminRoutingModule,
