a4f1a570057bc654beb97b25fb4a15133b939685
Kevin <bastokevindaniel@gmail.com>
2021-10-19 23:18:18 -0500

    edit novel form updated to support the new novel format

--- src/app/infra/controllers/admin/catalogues/edit-novel-form/edit-novel-form.component.ts
+++ src/app/infra/controllers/admin/catalogues/edit-novel-form/edit-novel-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Staff } from '../../../../../core/interfaces/staff';
+import { EditNovelFormService } from './edit-novel-form.service';
 
 @Component({
   selector: 'app-edit-novel-form',
@@ -7,9 +11,104 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditNovelFormComponent implements OnInit {
 
-  constructor() { }
+  public novelForm : FormGroup;
+  public staff : Array<Staff>;
+  public uploading : boolean;
+
+  constructor(
+    private formBuilder : FormBuilder,
+    public novelCreation : EditNovelFormService,
+    private router : Router
+  ) {
+    this.novelForm = this.formBuilder.group({
+      name : ["", [Validators.required]],
+      chapters  : [""],
+      status : ["", [Validators.required]],
+      author : ["", [Validators.required]],
+      genre : [""],
+      translators : this.formBuilder.array([])
+    })
+    this.uploading = false;
+  }
 
   ngOnInit(): void {
+    this.setUnchangeableData();
+    this.novelCreation.getStaff()
+    .then(staff => this.staff = staff)
+  }
+
+  submit(){
+
+  }
+  
+  setUnchangeableData(){
+    // setting unchangable data
+    this.setChaptersData();
+    this.setGenreData();
+    this.setTranslatorData();
+  }
+
+  private setChaptersData(){
+    this.chapters.setValue(0);
+    this.chapters.disable();
+    
+  }
+
+  private setGenreData(){
+    let genre = this.router.url.split("/").filter(element => {
+      if(element != "")
+        return element
+    })[1];
+    this.genre.setValue(genre);
+    this.genre.disable();
+  }
+
+  private setTranslatorData(){
+    this.addTranslator();
+    this.novelCreation.getUid().then(uid => {
+      this.translators.at(0).get('translator').setValue(uid)
+      this.translators.at(0).disable();
+    })
+  }
+
+  
+  get name(){
+    return this.novelForm.get('name')
+  }
+
+  get chapters(){
+    return this.novelForm.get('chapters')
+  }
+
+  get status(){
+    return this.novelForm.get('status')
+  }
+
+  get author(){
+    return this.novelForm.get('author')
+  }
+
+  get genre(){
+    return this.novelForm.get('genre')
+  }
+
+  get translators(){
+    return this.novelForm.get('translators') as FormArray;
+  }
+
+  addTranslator(){
+    const translator = this.formBuilder.group({
+      translator : ["", [Validators.required]]
+    });
+    this.translators.push(translator)
+  }
+
+  deleteTranslator(i : any){
+    this.translators.removeAt(i)
+  }
+
+  goBack(){
+    this.router.navigate([`/admin/${this.genre.value}`])
   }
 
 }
--- src/app/infra/controllers/admin/catalogues/edit-novel-form/edit-novel-form.service.ts
+++ src/app/infra/controllers/admin/catalogues/edit-novel-form/edit-novel-form.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { take } from 'rxjs/operators';
+import { Staff } from '../../../../../core/interfaces/staff';
+import { User } from '../../../../../core/interfaces/user.interface';
+import { NovelsRepo } from '../../../../../core/repos/novels.repo';
+import { NOVELS_REPO } from '../../../../repos/tokens';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditNovelFormService {
+  constructor(
+    @Inject(NOVELS_REPO) private novelsRepo: NovelsRepo,
+    private firestore: AngularFirestore,
+    private afauth : AngularFireAuth,
+    private router : Router
+  ) {}
 
-  constructor() { }
+
+
+  getStaff() : Promise<Array<Staff>>{
+    return new Promise((resolve, reject) => {
+      this.firestore
+      .collection<User>('users', (fn) => fn.where('role', '!=', 'reader'))
+      .valueChanges()
+      .pipe(take(1))
+      .toPromise()
+      .then((users) => {
+        let staff : Array<Staff> = [];
+        users.map(user => {
+          staff.push({
+            username: user.username,
+            uid : user.url
+          })
+        })
+        resolve(staff);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    })
+  }
+
+  public getUid() : Promise<string>{
+    return new Promise<string>((resolve, reject) => {
+      this.afauth.authState.pipe(take(1)).toPromise()
+    .then(user => {
+      resolve(user.uid);
+    })
+    })
+  }
 }
