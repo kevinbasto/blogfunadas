3a0340fbdc2799ec9bef0fb9d65888ca773abb39
Kevin <bastokevindaniel@gmail.com>
2021-10-19 21:01:28 -0500

    minor tweaks in the genres service

--- src/app/infra/controllers/admin/genres/genres.service.ts
+++ src/app/infra/controllers/admin/genres/genres.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { inject } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { Genre } from '../../../../core/interfaces/genre.interface';
 import { SystemMessage } from '../../../../core/interfaces/system-message';
 import { GenresRepo } from '../../../../core/repos/genres.repo';
@@ -14,14 +15,19 @@ export class GenresService {
 
   constructor(
     @Inject(GENRES_REPO) private genresRepo : GenresRepo,
-    @Inject(SidebarServiceToken) sidebar : Sidebar
+    @Inject(SidebarServiceToken) private sidebar : Sidebar,
+    private router : Router
   ) { }
 
   createGenre(genre : Genre) : Promise<SystemMessage>{
     return new Promise<SystemMessage>((resolve, reject) => {
       genre.name = this.despacer(genre.name);
       this.genresRepo.createGenre(genre)
-      .then(res => resolve(res))
+      .then(res => {
+        resolve(res);
+        this.sidebar.deleteSidebarElements();
+        this.router.navigate(['/admin/genres']);
+      })
       .catch(err => reject(err))
     })
   }
