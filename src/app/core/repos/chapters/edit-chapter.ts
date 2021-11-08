c75f5d64debeb19d7f489c5ba34ebb9c1e5979cb
Kevin <bastokevindaniel@gmail.com>
2021-11-07 19:04:30 -0600

    implemented the interface for the concrete app

--- src/app/core/repos/chapters/edit-chapter.ts
+++ src/app/core/repos/chapters/edit-chapter.ts
@@ -1,5 +1,5 @@
 import { ChapterContent } from "../../interfaces/chapter.interface";
 
 export interface editChapterRepo{
-    editChapter(genre : string, novel : string, chapter : string, chapterContent : ChapterContent) : Promise<void>
+    editChapter(genre : string, novel : string, chapter : string, chapterContent : ChapterContent) : Promise<void>;
 }
--- src/app/infra/repos/chapters/edit-chapter/edit-chapter.service.ts
+++ src/app/infra/repos/chapters/edit-chapter/edit-chapter.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
+import { ChapterContent } from '../../../../core/interfaces/chapter.interface';
+import { editChapterRepo } from '../../../../core/repos/chapters/edit-chapter';
 
 @Injectable()
-export class EditChapterService {
+export class EditChapterService implements editChapterRepo{
 
   constructor() { }
+
+  async editChapter(genre : string, novel : string, chapter : string, chapterContent : ChapterContent) : Promise<void> {
+    try {
+      
+    } catch (error) {
+      throw error;
+    }
+  }
 }
